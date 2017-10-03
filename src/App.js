ee03fc8cd3512931c918dad6047cae09208c9f50
Erin Witmer <ecwitmer@gmail.com>
2017-10-02 21:12:21 -0400

    updates to font and favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
 import Hero from './components/hero.js'
 import Projects from './components/projects.js'
 import ProjectHeader from './components/project-header.js'
+import ProjectFooter from './components/project-footer.js'
 import Footer from './components/footer.js'
 import Quote from './components/quote.js'
 
@@ -14,6 +14,7 @@ class App extends Component {
         <Quote />
         <ProjectHeader />
         <Projects />
+        <ProjectFooter />
         <Footer />
       </div>
     );
